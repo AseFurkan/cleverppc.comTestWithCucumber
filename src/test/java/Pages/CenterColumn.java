3183c9a6f5fc33ae78ee0d0355dd3160caac01a1
ZeynepBaglars <zeynepbaglars@gmail.com>
2023-09-23 23:15:12 +0300

    başlangıç-2

--- src/test/java/Pages/CenterColumn.java
+++ src/test/java/Pages/CenterColumn.java
@@ -1,4 +1,11 @@
 package Pages;
 
+import Utilities.GWD;
+import org.openqa.selenium.support.PageFactory;
+
 public class CenterColumn {
+    public CenterColumn() {
+        PageFactory.initElements(GWD.getDriver(), this);
+
+    }
 }
